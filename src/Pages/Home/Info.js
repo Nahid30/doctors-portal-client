8be9f4c8c61245ac2c24f206c808e59ffbe1eb1f
Nahid30 <nahidshuvo30@gmail.com>
2022-05-13 02:56:06 +0600

    create a new page appointment and added footer in it. set max width in app.js for large devices and footer sent into shared components.

--- src/Pages/Home/Info.js
+++ src/Pages/Home/Info.js
@@ -11,7 +11,7 @@ const Info = () => {
         <div className='grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 '>
             <InfoCart cardText="9:00AM - 8:00PM" cardTittle="Opening Hours" bgClass="bg-gradient-to-r from-secondary to-primary" img={clock}></InfoCart>
 
-            <InfoCart cardText="Brooklyn, NY 10036, United States" cardTittle="Visit Our Locations" bgClass="bg-accent" img={marker}></InfoCart>
+            <InfoCart cardText="Brooklyn, NY 10036, United States" cardTittle="Visit Our Locations" bgClass="bg-[#3A4256]" img={marker}></InfoCart>
 
             <InfoCart cardText="+000 123 456789" cardTittle="Contact Us" bgClass="bg-gradient-to-r from-secondary to-primary" img={phone}></InfoCart>
         </div>
--- src/Pages/Shared/Footer.js
+++ src/Pages/Shared/Footer.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import footer from '../../assets/images/footer.png';
 
 const Footer = () => {
